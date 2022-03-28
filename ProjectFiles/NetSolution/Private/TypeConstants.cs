b774b18448abb518c9f33217925955b861de5316
s.mori@asem.it <simmor-asem>
2022-03-28 18:28:40 +0200

    push

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,23 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace Sample01
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Sample01");
+        public static readonly NodeId Screen_3 = new NodeId(namespaceIndex, new Guid("12d8a1dc4b799e2e97ba62b60d327f2f"));
+        public static readonly NodeId Screen_1 = new NodeId(namespaceIndex, new Guid("df68ed1522962b4418ace5227ec2acdd"));
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("eeaf178b88a96872317241fc9f561e39"));
+        public static readonly NodeId Screen_2 = new NodeId(namespaceIndex, new Guid("8ec30ac82766b7a1e410a182bc7df689"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Sample01");
+    }
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,26 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "Sample01", Guid = "12d8a1dc4b799e2e97ba62b60d327f2f")]
+public class Screen_3 : QPlatform.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Sample01", Guid = "df68ed1522962b4418ace5227ec2acdd")]
+public class Screen_1 : QPlatform.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Sample01", Guid = "eeaf178b88a96872317241fc9f561e39")]
+public class MainWindow : QPlatform.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "Sample01", Guid = "8ec30ac82766b7a1e410a182bc7df689")]
+public class Screen_2 : QPlatform.UI.Panel
+{
+}
